fb7c50927ab127367b9bfd7c50ee9ba8785fbdc5
elias.gabrielsson <elias.gabrielsson@9da9fe1e-bf8e-4723-8f12-9f09a4da59e9>
2013-04-19 09:18:40 +0000

    git-svn-id: https://subversion.assembla.com/svn/eda095-projekt@4 9da9fe1e-bf8e-4723-8f12-9f09a4da59e9

--- trunk/src/server/ServerSocketThread.java
+++ trunk/src/server/ServerSocketThread.java
@@ -1,14 +1,35 @@
 package server;
 
-public class ServerSocketThread implements Runnable {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
 
+public class ServerSocketThread implements Runnable {
+	private Socket s = null;
+	BufferedWriter writer = null;
+	BufferedReader reader = null;
+	
+	public ServerSocketThread(Socket s) {
+		this.s = s;
+		try {
+			reader = new BufferedReader(new InputStreamReader(s.getInputStream()));
+			writer = new BufferedWriter(new OutputStreamWriter(s.getOutputStream()));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-
-	}
-	private void waitForCommand(){
 		
+	}
+	
+	private void waitForCommand(){
+		while(true){
+			
+		}
 	}
 	
 
