08e1a3dccd4b6ce4db37e93bc07829a4314ce859
SnyggareBob <SnyggareBob@9da9fe1e-bf8e-4723-8f12-9f09a4da59e9>
2013-04-24 09:57:13 +0000

    bajs
    git-svn-id: https://subversion.assembla.com/svn/eda095-projekt@26 9da9fe1e-bf8e-4723-8f12-9f09a4da59e9

--- trunk/src/common/server/Write.java
+++ trunk/src/common/server/Write.java
@@ -1,6 +1,5 @@
 package common.server;
 
-
 public class Write extends ServerCommand {
 
 	public Write(String[] arg) {
---
+++ trunk/src/server/ServerDocHandler.java
@@ -0,0 +1,22 @@
+package server;
+
+import java.util.List;
+
+public class ServerDocHandler {
+		
+	public ServerDocHandler(){
+		
+	}
+	public static synchronized ServerDoc getDoc(String filename){
+		return null;	
+	}
+	
+	public static synchronized void save(){
+		
+	}
+	
+	public List<String> getDocList(){
+		return null;
+		
+	}
+}
