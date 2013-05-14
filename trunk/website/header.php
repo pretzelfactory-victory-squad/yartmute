19d2a1303603c59c99cc6f49f370fe2936f2ba7c
bobbarebygg <bobbarebygg@9da9fe1e-bf8e-4723-8f12-9f09a4da59e9>
2013-05-14 16:34:22 +0000

    Added website stubs
    git-svn-id: https://subversion.assembla.com/svn/eda095-projekt@101 9da9fe1e-bf8e-4723-8f12-9f09a4da59e9

---
+++ trunk/website/header.php
@@ -0,0 +1,9 @@
+<header>
+<nav>
+<ul>
+<li><a href="#">Projekt</a></li>
+<li><a href="#">Manual</a></li>
+<li><a href="#">Downloads</a></li>
+<li><a href="#">Om oss</a></li>
+</nav>
+</header>
---
+++ trunk/website/index.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+</head>
+<body>
+<?php include("header.php"); ?>
+<section>
+
+</section>
+<?php include("footer.php"); ?>
+</body>
+</html>
